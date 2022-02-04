b6c721068590bcd5060d58a76952dc5a86317d3d
TheJhonni <davideconi91@gmail.com>
2022-02-04 09:33:50 +0100

    setting up react-app + Tailwind Css

---
+++ weather/src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <h1 className="cursor-pointer">Im h1 biatch</h1>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
